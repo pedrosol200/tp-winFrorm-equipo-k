fb842e94990878c164f795953663b8517974ef23
Pedro Soliz <pedrosoliz09@gmail.com>
2026-09-16 15:07:35 -0300

    Agregar archivos de proyecto.

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+
+        public Articulo()
+        {
+            Imagenes = new List<Imagen>();
+        }
+
+    }
+}
